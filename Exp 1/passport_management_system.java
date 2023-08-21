c4f3b816a005aa127a5e8fe056c39b7f2711a64a
EswarreddyEnimireddy <132686075+EswarreddyEnimireddy@users.noreply.github.com>
2023-08-21 13:51:03 +0530

    Add files via upload

---
+++ Exp 1/passport_management_system.java
@@ -0,0 +1,48 @@
+
+
+/**
+ * Class passport_management_system
+ */
+public class passport_management_system {
+
+  //
+  // Fields
+  //
+
+  public char verify_detail;
+  
+  //
+  // Constructors
+  //
+  public passport_management_system () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of verify_detail
+   * @param newVar the new value of verify_detail
+   */
+  public void setVerify_detail (char newVar) {
+    verify_detail = newVar;
+  }
+
+  /**
+   * Get the value of verify_detail
+   * @return the value of verify_detail
+   */
+  public char getVerify_detail () {
+    return verify_detail;
+  }
+
+  //
+  // Other methods
+  //
+
+}
