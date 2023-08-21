a9b3ffe8fc8710addfa86d1ea50d2e2d36e77760
EswarreddyEnimireddy <132686075+EswarreddyEnimireddy@users.noreply.github.com>
2023-08-21 20:53:51 +0530

    Add files via upload

---
+++ Exp 13/Dealer.java
@@ -0,0 +1,89 @@
+
+
+/**
+ * Class Dealer
+ */
+public class Dealer {
+
+  //
+  // Fields
+  //
+
+  private void Name;
+  private void Availability;
+  
+  //
+  // Constructors
+  //
+  public Dealer () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of Name
+   * @param newVar the new value of Name
+   */
+  private void setName (void newVar) {
+    Name = newVar;
+  }
+
+  /**
+   * Get the value of Name
+   * @return the value of Name
+   */
+  private void getName () {
+    return Name;
+  }
+
+  /**
+   * Set the value of Availability
+   * @param newVar the new value of Availability
+   */
+  private void setAvailability (void newVar) {
+    Availability = newVar;
+  }
+
+  /**
+   * Get the value of Availability
+   * @return the value of Availability
+   */
+  private void getAvailability () {
+    return Availability;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   * @return       Dealer
+   */
+  public Dealer payment()
+  {
+  }
+
+
+  /**
+   * @return       Dealer
+   */
+  public Dealer delivery()
+  {
+  }
+
+
+  /**
+   * @return       Dealer
+   */
+  public Dealer dealer()
+  {
+  }
+
+
+}
---
+++ Exp 13/customer1.java
@@ -0,0 +1,89 @@
+
+
+/**
+ * Class customer1
+ */
+public class customer1 {
+
+  //
+  // Fields
+  //
+
+  private void Name;
+  private void purchase_product;
+  
+  //
+  // Constructors
+  //
+  public customer1 () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of Name
+   * @param newVar the new value of Name
+   */
+  private void setName (void newVar) {
+    Name = newVar;
+  }
+
+  /**
+   * Get the value of Name
+   * @return the value of Name
+   */
+  private void getName () {
+    return Name;
+  }
+
+  /**
+   * Set the value of purchase_product
+   * @param newVar the new value of purchase_product
+   */
+  private void setPurchase_product (void newVar) {
+    purchase_product = newVar;
+  }
+
+  /**
+   * Get the value of purchase_product
+   * @return the value of purchase_product
+   */
+  private void getPurchase_product () {
+    return purchase_product;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   * @return       customer1
+   */
+  public customer1 payment()
+  {
+  }
+
+
+  /**
+   * @return       customer1
+   */
+  public customer1 feedback()
+  {
+  }
+
+
+  /**
+   * @return       customer1
+   */
+  public customer1 customer()
+  {
+  }
+
+
+}
