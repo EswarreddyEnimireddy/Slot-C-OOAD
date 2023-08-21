32bc9db78878d794da9c1a54c71156f121aff11a
EswarreddyEnimireddy <132686075+EswarreddyEnimireddy@users.noreply.github.com>
2023-08-21 20:31:41 +0530

    Add files via upload

---
+++ Exp 9/Internet.java
@@ -0,0 +1,114 @@
+
+
+/**
+ * Class Internet
+ */
+public class Internet {
+
+  //
+  // Fields
+  //
+
+  private void enetr_id;
+  private void download;
+  private void login;
+  private void logout;
+  
+  //
+  // Constructors
+  //
+  public Internet () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of enetr_id
+   * @param newVar the new value of enetr_id
+   */
+  private void setEnetr_id (void newVar) {
+    enetr_id = newVar;
+  }
+
+  /**
+   * Get the value of enetr_id
+   * @return the value of enetr_id
+   */
+  private void getEnetr_id () {
+    return enetr_id;
+  }
+
+  /**
+   * Set the value of download
+   * @param newVar the new value of download
+   */
+  private void setDownload (void newVar) {
+    download = newVar;
+  }
+
+  /**
+   * Get the value of download
+   * @return the value of download
+   */
+  private void getDownload () {
+    return download;
+  }
+
+  /**
+   * Set the value of login
+   * @param newVar the new value of login
+   */
+  private void setLogin (void newVar) {
+    login = newVar;
+  }
+
+  /**
+   * Get the value of login
+   * @return the value of login
+   */
+  private void getLogin () {
+    return login;
+  }
+
+  /**
+   * Set the value of logout
+   * @param newVar the new value of logout
+   */
+  private void setLogout (void newVar) {
+    logout = newVar;
+  }
+
+  /**
+   * Get the value of logout
+   * @return the value of logout
+   */
+  private void getLogout () {
+    return logout;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   * @return       Internet
+   */
+  public Internet verify()
+  {
+  }
+
+
+  /**
+   */
+  public void Status()
+  {
+  }
+
+
+}
---
+++ Exp 9/User1.java
@@ -0,0 +1,73 @@
+
+
+/**
+ * Class User1
+ */
+public class User1 {
+
+  //
+  // Fields
+  //
+
+  private void login;
+  private void logout;
+  
+  //
+  // Constructors
+  //
+  public User1 () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of login
+   * @param newVar the new value of login
+   */
+  private void setLogin (void newVar) {
+    login = newVar;
+  }
+
+  /**
+   * Get the value of login
+   * @return the value of login
+   */
+  private void getLogin () {
+    return login;
+  }
+
+  /**
+   * Set the value of logout
+   * @param newVar the new value of logout
+   */
+  private void setLogout (void newVar) {
+    logout = newVar;
+  }
+
+  /**
+   * Get the value of logout
+   * @return the value of logout
+   */
+  private void getLogout () {
+    return logout;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   * @return       User1
+   */
+  public User1 surf_books()
+  {
+  }
+
+
+}
