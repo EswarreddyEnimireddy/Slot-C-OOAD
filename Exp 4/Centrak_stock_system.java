1170d487c1317d6c0a8945da1e239c67e459f752
EswarreddyEnimireddy <132686075+EswarreddyEnimireddy@users.noreply.github.com>
2023-08-21 20:26:00 +0530

    Add files via upload

---
+++ Exp 4/Centrak_stock_system.java
@@ -0,0 +1,64 @@
+
+
+/**
+ * Class Centrak_stock_system
+ */
+public class Centrak_stock_system {
+
+  //
+  // Fields
+  //
+
+  private void _store_stock_detail;
+  
+  //
+  // Constructors
+  //
+  public Centrak_stock_system () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of _store_stock_detail
+   * @param newVar the new value of _store_stock_detail
+   */
+  private void set_store_stock_detail (void newVar) {
+    _store_stock_detail = newVar;
+  }
+
+  /**
+   * Get the value of _store_stock_detail
+   * @return the value of _store_stock_detail
+   */
+  private void get_store_stock_detail () {
+    return _store_stock_detail;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   * @return       Centrak stock system
+   */
+  public Centrak stock system print_bill()
+  {
+  }
+
+
+  /**
+   * @return       Centrak stock system
+   */
+  public Centrak stock system delivery_product()
+  {
+  }
+
+
+}
---
+++ Exp 4/Cus_to.java
@@ -0,0 +1,56 @@
+
+
+/**
+ * Class Cus_to
+ */
+public class Cus_to {
+
+  //
+  // Fields
+  //
+
+  private void place_order;
+  
+  //
+  // Constructors
+  //
+  public Cus_to () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of place_order
+   * @param newVar the new value of place_order
+   */
+  private void setPlace_order (void newVar) {
+    place_order = newVar;
+  }
+
+  /**
+   * Get the value of place_order
+   * @return the value of place_order
+   */
+  private void getPlace_order () {
+    return place_order;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   * @return       Cus to
+   */
+  public Cus to payment()
+  {
+  }
+
+
+}
