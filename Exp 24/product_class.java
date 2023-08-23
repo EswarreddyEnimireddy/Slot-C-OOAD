ef6fcc39f8ca82025aff719648065cd2cf1f0bed
EswarreddyEnimireddy <132686075+EswarreddyEnimireddy@users.noreply.github.com>
2023-08-23 10:00:14 +0530

    Add files via upload

---
+++ Exp 24/product_class.java
@@ -0,0 +1,31 @@
+
+
+/**
+ * Class product_class
+ */
+public class product_class {
+
+  //
+  // Fields
+  //
+
+  
+  //
+  // Constructors
+  //
+  public product_class () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  //
+  // Other methods
+  //
+
+}
