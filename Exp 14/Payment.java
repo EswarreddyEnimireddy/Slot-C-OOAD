6753ee621e89ff427fb605c184feb8067575280b
EswarreddyEnimireddy <132686075+EswarreddyEnimireddy@users.noreply.github.com>
2023-08-23 08:27:11 +0530

    Add files via upload

---
+++ Exp 14/Payment.java
@@ -0,0 +1,98 @@
+
+import java.util.*;
+
+
+/**
+ * Class Payment
+ */
+public class Payment {
+
+  //
+  // Fields
+  //
+
+  private void id;
+  private void card_no;
+  private void amount;
+  
+  //
+  // Constructors
+  //
+  public Payment () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of id
+   * @param newVar the new value of id
+   */
+  private void setId (void newVar) {
+    id = newVar;
+  }
+
+  /**
+   * Get the value of id
+   * @return the value of id
+   */
+  private void getId () {
+    return id;
+  }
+
+  /**
+   * Set the value of card_no
+   * @param newVar the new value of card_no
+   */
+  private void setCard_no (void newVar) {
+    card_no = newVar;
+  }
+
+  /**
+   * Get the value of card_no
+   * @return the value of card_no
+   */
+  private void getCard_no () {
+    return card_no;
+  }
+
+  /**
+   * Set the value of amount
+   * @param newVar the new value of amount
+   */
+  private void setAmount (void newVar) {
+    amount = newVar;
+  }
+
+  /**
+   * Get the value of amount
+   * @return the value of amount
+   */
+  private void getAmount () {
+    return amount;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void add()
+  {
+  }
+
+
+  /**
+   */
+  public void update()
+  {
+  }
+
+
+}
