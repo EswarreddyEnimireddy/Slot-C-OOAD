a1e65953706ace37021fd7c3937fb049086ff85d
EswarreddyEnimireddy <132686075+EswarreddyEnimireddy@users.noreply.github.com>
2023-08-23 09:58:09 +0530

    Add files via upload

---
+++ Exp 22/Insurance_class.java
@@ -0,0 +1,180 @@
+
+import java.util.*;
+
+
+/**
+ * Class Insurance_class
+ */
+public class Insurance_class {
+
+  //
+  // Fields
+  //
+
+  private void id;
+  private void plan;
+  private void description;
+  private void number;
+  private void type;
+  private void premium;
+  private void amount;
+  
+  //
+  // Constructors
+  //
+  public Insurance_class () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of id
+   * @param newVar the new value of id
+   */
+  private void setId (void newVar) {
+    id = newVar;
+  }
+
+  /**
+   * Get the value of id
+   * @return the value of id
+   */
+  private void getId () {
+    return id;
+  }
+
+  /**
+   * Set the value of plan
+   * @param newVar the new value of plan
+   */
+  private void setPlan (void newVar) {
+    plan = newVar;
+  }
+
+  /**
+   * Get the value of plan
+   * @return the value of plan
+   */
+  private void getPlan () {
+    return plan;
+  }
+
+  /**
+   * Set the value of description
+   * @param newVar the new value of description
+   */
+  private void setDescription (void newVar) {
+    description = newVar;
+  }
+
+  /**
+   * Get the value of description
+   * @return the value of description
+   */
+  private void getDescription () {
+    return description;
+  }
+
+  /**
+   * Set the value of number
+   * @param newVar the new value of number
+   */
+  private void setNumber (void newVar) {
+    number = newVar;
+  }
+
+  /**
+   * Get the value of number
+   * @return the value of number
+   */
+  private void getNumber () {
+    return number;
+  }
+
+  /**
+   * Set the value of type
+   * @param newVar the new value of type
+   */
+  private void setType (void newVar) {
+    type = newVar;
+  }
+
+  /**
+   * Get the value of type
+   * @return the value of type
+   */
+  private void getType () {
+    return type;
+  }
+
+  /**
+   * Set the value of premium
+   * @param newVar the new value of premium
+   */
+  private void setPremium (void newVar) {
+    premium = newVar;
+  }
+
+  /**
+   * Get the value of premium
+   * @return the value of premium
+   */
+  private void getPremium () {
+    return premium;
+  }
+
+  /**
+   * Set the value of amount
+   * @param newVar the new value of amount
+   */
+  private void setAmount (void newVar) {
+    amount = newVar;
+  }
+
+  /**
+   * Get the value of amount
+   * @return the value of amount
+   */
+  private void getAmount () {
+    return amount;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void add()
+  {
+  }
+
+
+  /**
+   */
+  public void edit()
+  {
+  }
+
+
+  /**
+   */
+  public void delete()
+  {
+  }
+
+
+  /**
+   */
+  public void search()
+  {
+  }
+
+
+}
---
+++ Exp 22/Permission_class.java
@@ -0,0 +1,129 @@
+
+import java.util.*;
+
+
+/**
+ * Class Permission_class
+ */
+public class Permission_class {
+
+  //
+  // Fields
+  //
+
+  private void id;
+  private void role__id;
+  private void module;
+  private void description;
+  
+  //
+  // Constructors
+  //
+  public Permission_class () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of id
+   * @param newVar the new value of id
+   */
+  private void setId (void newVar) {
+    id = newVar;
+  }
+
+  /**
+   * Get the value of id
+   * @return the value of id
+   */
+  private void getId () {
+    return id;
+  }
+
+  /**
+   * Set the value of role__id
+   * @param newVar the new value of role__id
+   */
+  private void setRole__id (void newVar) {
+    role__id = newVar;
+  }
+
+  /**
+   * Get the value of role__id
+   * @return the value of role__id
+   */
+  private void getRole__id () {
+    return role__id;
+  }
+
+  /**
+   * Set the value of module
+   * @param newVar the new value of module
+   */
+  private void setModule (void newVar) {
+    module = newVar;
+  }
+
+  /**
+   * Get the value of module
+   * @return the value of module
+   */
+  private void getModule () {
+    return module;
+  }
+
+  /**
+   * Set the value of description
+   * @param newVar the new value of description
+   */
+  private void setDescription (void newVar) {
+    description = newVar;
+  }
+
+  /**
+   * Get the value of description
+   * @return the value of description
+   */
+  private void getDescription () {
+    return description;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void add()
+  {
+  }
+
+
+  /**
+   */
+  public void edit()
+  {
+  }
+
+
+  /**
+   */
+  public void search()
+  {
+  }
+
+
+  /**
+   */
+  public void delete()
+  {
+  }
+
+
+}
